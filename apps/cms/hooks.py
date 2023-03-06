f63638788ccaf60f8494c6c05c7cd1141c309c49
XavierXinchi <Xinchi.Shi20@gmail.com>
2023-03-06 19:50:49 +0000

    add hooks.py

---
+++ apps/cms/hooks.py
@@ -0,0 +1,12 @@
+from .views import bp
+from .models import CMSUser
+from flask import session, g
+import config
+
+@bp.before_request
+def before_request():
+    if config.CMS_USER_ID in session:
+        user_id = session.get(config.CMS_USER_ID)
+        user = CMSUser.query.get(user_id)
+        if user:
+            g.cms_user = user
