aa325417ab3d4f0caf95b32bd80213623ba914da
XavierXinchi <Xinchi.Shi20@gmail.com>
2023-03-17 17:14:31 +0000

    Defined cms permission and role model

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -2,6 +2,39 @@ from exts import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+class CMSPersmission(object):
+    # The binary representation of 255: 1111 1111
+    ALL_PERMISSION = 0b11111111
+    # 1. 访问者权限 Visitor Permissions
+    VISITOR =        0b00000001
+    # 2. 管理帖子权限 Permission to manage posts
+    POSTER =         0b00000010
+    # 3. 管理评论的权限 Permission to manage comments
+    COMMENTER =      0b00000100
+    # 4. 管理板块的权限 Permission to manage the board
+    BOARDER =        0b00001000
+    # 5. 管理前台用户的权限 Manage frontend user permissions
+    FRONTUSER =      0b00010000
+    # 6. 管理后台用户的权限 Manage backend user permissions
+    CMSUSER =        0b00100000
+    # 7. 管理后台管理员的权限 Manage the permission of the backend administrator
+    ADMINER =        0b01000000
+
+cms_role_user = db.Table(
+    'cms_role_user',
+    db.Column('cms_role_id',db.Integer,db.ForeignKey('cms_role.id'),primary_key=True),
+    db.Column('cms_user_id',db.Integer,db.ForeignKey('cms_user.id'),primary_key=True)
+)
+
+class CMSRole(db.Model):
+    __tablename__ = 'cms_role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(200),nullable=True)
+    create_time = db.Column(db.DateTime,default=datetime.now)
+    permissions = db.Column(db.Integer,default=CMSPersmission.VISITOR)
+
+    users = db.relationship('CMSUser',secondary=cms_role_user,backref='roles')
 
 class CMSUser(db.Model):
     __tablename__ = 'cms_user'
--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ from exts import db
 from apps.cms import models as cms_models
 
 CMSUser = cms_models.CMSUser
+CMSRole = cms_models.CMSRole
+CMSPermission = cms_models.CMSPersmission
 
 app = create_app()
 
@@ -12,6 +14,7 @@ manager = Manager(app)
 
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
 
 @manager.option('-u', '--username', dest='username')
 @manager.option('-p', '--password', dest='password')
@@ -21,6 +24,29 @@ def create_cms_user(username, password, email):
     db.session.add(user)
     db.session.commit()
     print('CMS User created successfully!')
+
+
+@manager.command
+def create_role():
+    # 1. Visitors (can change personal information)
+    visitor = CMSRole(name='visitor', desc='can only change personal information')
+    visitor.permissions = CMSPermission.VISITOR
+
+    # 2. Operation roles (modify personal information, manage posts, manage comments, manage front desk users)
+    operator = CMSRole(name='operation_specialist', desc='Manage posts, manage comments, manage front desk users')
+    operator.permissions = CMSPermission.VISITOR | CMSPermission.POSTER | CMSPermission.CMSUSER | CMSPermission.COMMENTER | CMSPermission.FRONTUSER
+
+    # 3. Administrators (with most of the privileges)
+    admin = CMSRole(name='administrator', desc='Have all permissions for this system')
+    admin.permissions = CMSPermission.VISITOR | CMSPermission.POSTER | CMSPermission.CMSUSER | CMSPermission.COMMENTER | CMSPermission.FRONTUSER | CMSPermission.BOARDER
+
+    # 4. Developer (all permissions)
+    developer = CMSRole(name='developer', desc='Developer-only role')
+    developer.permissions = CMSPermission.ALL_PERMISSION
+
+    db.session.add_all([visitor, operator, admin, developer])
+    db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
