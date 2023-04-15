17ce0c26168a426d9e9bae4611ed64d9e20d98b3
XavierXinchi <Xinchi.Shi20@gmail.com>
2023-04-16 00:54:08 +0100

    update requirements.txt

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'comp208'
 
 # Configuration variables for the database
-# 47.243.187.169
+#  127.0.0.1 47.243.187.169
 HOSTNAME = '47.243.187.169' 
 PORT     = '3306'
 DATABASE = 'serendipity'
