b0614242ddb5e6cdc46e73512c2ee0a0d159642c
XavierXinchi <Xinchi.Shi20@gmail.com>
2023-04-17 00:22:49 +0100

    update config

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ HOSTNAME = '127.0.0.1'
 PORT     = '3306' # 3306
 DATABASE = 'serendipity'
 USERNAME = 'root'
-PASSWORD = 'SXc2002627SXc'
+PASSWORD = 'seren' # SXc2002627SXc
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
