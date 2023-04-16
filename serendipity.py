b7278f469fc2cfe38e5cf4e790bfcc916906f54c
XavierXinchi <Xinchi.Shi20@gmail.com>
2023-04-16 19:08:44 +0100

    update serendipity.py

--- serendipity.py
+++ serendipity.py
@@ -19,6 +19,8 @@ def create_app():
     # CSRFProtect(app)
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
     app = create_app()
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True) 
